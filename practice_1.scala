22c4d8f33418f8caa4e6c4bada59bb7aa60cbb5a
Christian Gandarilla <christian.gandarilla@gmail.com>
2023-09-27 19:54:46 -0700

    file to preactice 1

---
+++ practice_1.scala
@@ -0,0 +1,11 @@
+// Assessment 1/Practica 1
+//1. Desarrollar un algoritmo en scala que calcule el radio de un circulo
+//2. Desarrollar un algoritmo en scala que me diga si un numero es primo
+//3. Dada la variable bird = "tweet", utiliza interpolacion de string para
+//   imprimir "Estoy ecribiendo un tweet"
+//4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slilce para extraer la
+//   secuencia "Luke"
+//5. Cual es la diferencia entre value y una variable en scala?
+//6. Dada la tupla (2,4,5,1,2,3,3.1416,23) regresa el numero 3.1416 
+
+// Team 1 - Christian Gandarilla && Galilea Jarillo
