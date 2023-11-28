cf4c988c14090fe1bd1aa558205978afcd2a1069
GalileaJarillo <a1206207@uabc.edu.mx>
2023-11-28 11:33:10 -0800

    updated line 37

--- Final Project/project.scala
+++ Final Project/project.scala
@@ -1,8 +1,8 @@
-/*import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.IndexToString
 import org.apache.spark.ml.feature.StringIndexer
-import org.apache.spark.ml.feature.VectorIndexer*/
+import org.apache.spark.ml.feature.VectorIndexer
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.feature.VectorAssembler
