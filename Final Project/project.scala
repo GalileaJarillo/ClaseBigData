d990144d06900a60f14d160c0d5fed405981d74f
Christian Gandarilla <christian.gandarilla@gmail.com>
2023-11-27 22:13:13 -0800

    Working...

--- Final Project/project.scala
+++ Final Project/project.scala
@@ -14,3 +14,40 @@ data.columns.foreach(println)
 data.printSchema()
 data.show(5)
 data.describe().show()
+
+val bank = data.withColumn("age", col("age").cast("Double")).withColumn("balance", col("balance").cast("Double")).withColumn("marital2", col("marital2").cast("Double")).withColumn("education2", col("education2").cast("Double")).withColumn("poutcome2", col("poutcome2").cast("Double"))
+
+bank.printSchema()
+
+
+
+// No sabemos si lo ocupamos...
+// val assembler = new VectorAssembler().setInputCols(Array("age","marital2","balance","education2","poutcome2")).setOutputCol("features")
+
+// val features = assembler.transform(bank)
+
+// features.printSchema()
+
+
+// val indexerLabel = new StringIndexer().setInputCol("job").setOutputCol("indexedLabel").fit(features)
+
+// val indexerFeatures = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(4)
+
+// val Array(training, test) = features.randomSplit(Array(0.7, 0.3), seed = 12345)  
+
+// val layers = Array[Int](4, 5, 4, 3)
+
+// val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setLabelCol("indexedLabel").setFeaturesCol("indexedFeatures").setBlockSize(128).setSeed(12345).setMaxIter(100)
+
+
+// val converterLabel = new IndexToString().setInputCol("prediction").setOutputCol("predictedLabel").setLabels(indexerLabel.labels)
+
+// val pipeline = new Pipeline().setStages(Array(indexerLabel, indexerFeatures, trainer, converterLabel))
+
+// val model = pipeline.fit(training)
+// val results = model.transform(test)
+// results.show()
+
+// val evaluator = new MulticlassClassificationEvaluator().setLabelCol("indexedLabel").setPredictionCol("prediction").setMetricName("accuracy")
+
+// println(s"Prueba de Precision = ${evaluator.evaluate(results.select("prediction", "indexedLabel"))}")
